7251a365e17ae8339987b061256a10eb48e40721
Grey Keith <r.grey.keith@gmail.com>
2018-01-26 20:29:09 -0800

    trying to fix

--- js/ThreeDee.js
+++ js/ThreeDee.js
@@ -20,20 +20,20 @@ class ThreeDee {
     this.makeStars();
 
     new THREE.TextureLoader().load(
-      '../assets/space.jpg',
+      '../darkdreamzz/assets/space.jpg',
       (texture) => {
         this.buildSpace(texture)
       }
     );
 
     new THREE.TextureLoader().load(
-      '../assets/92k-moon-color-map-3d-model.jpg',
+      '../darkdreamzz/assets/92k-moon-color-map-3d-model.jpg',
       (texture) => {
         this.buildMoon(texture);
       }
     );
 
-    new THREE.FontLoader().load('../assets/Adine_Kirnberg_Regular.json', (f) => {
+    new THREE.FontLoader().load('../darkdreamzz/assets/Adine_Kirnberg_Regular.json', (f) => {
       this.buildFont(f, 'Dark');
       this.buildFont(f, 'Dreamzz');
       this['Dark'].position.x = -20;
@@ -44,7 +44,7 @@ class ThreeDee {
 
   makeStars() {
     const POINTS = 10000;
-    const sprite = new THREE.TextureLoader().load('../assets/star.png');
+    const sprite = new THREE.TextureLoader().load('../darkdreamzz/assets/star.png');
     const geo = new THREE.BufferGeometry();
     const positions = new Float32Array( POINTS * 3 );
     const sizes = new Float32Array( POINTS );
